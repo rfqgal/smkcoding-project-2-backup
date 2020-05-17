a3905772edc5b16498751de360d59a8672ca4c2e
Rifky Galuh <56381009+rfqgal@users.noreply.github.com>
2020-05-17 17:11:02 +0700

    SMK Coding Challenge 2
    Membuat aplikasi tentang Kesehatan (COVID-19) menggunakan Public APIs

--- app/src/main/java/com/example/smkcodingproject2challenge/FragmentCovid19.kt
+++ app/src/main/java/com/example/smkcodingproject2challenge/FragmentCovid19.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fragment_covid_19.*
 
 class FragmentCovid19: Fragment() {
 
@@ -22,5 +24,8 @@ class FragmentCovid19: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Glide.with(context!!)
+            .load("https://res.cloudinary.com/dk0z4ums3/image/upload/v1589160479/attached_image/virus-corona-0-alodokter.jpg")
+            .into(imgCovid19)
     }
 }
